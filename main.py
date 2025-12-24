e8e68c6a39b2d27e6e68ae544bc8c13ff5300450
dabid chu <arranzd5@gmail.com>
2025-12-24 13:03:54 +0100

    lo ultimo

--- main.py
+++ main.py
@@ -207,3 +207,13 @@ async def download_song_url(bg_tasks: BackgroundTasks, url: str = Form(...)):
         )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
